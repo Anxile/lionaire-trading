275b7946aecc4de36bfffe2e398c405f575b8e9f
Anxile <onebox.chen@outlook.com>
2025-05-14 14:31:12 -0400

    implement JWT authentication

---
+++ src/main/java/com/lionaire/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.lionaire.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "qwoidonclkncxznczcnmnqebfeiuwfrusamx";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER = "Authorization";
+    public static final long EXPIRATION_TIME = 864_000_000;
+}
