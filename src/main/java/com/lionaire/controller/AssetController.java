72110cf7f0a03a8b177105b9cc1bf9b7a37521bd
Anxile <onebox.chen@outlook.com>
2025-06-28 19:22:42 -0400

    fixed the error of AssetService not injected

--- src/main/java/com/lionaire/controller/AssetController.java
+++ src/main/java/com/lionaire/controller/AssetController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/asset")
 public class AssetController {
+    @Autowired
     private AssetService assetService;
     @Autowired
     private UserService userService;
